708802390fdd9cd87483004fdc7c3c1db85e0d4b
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2022-07-17 14:37:40 +0500

    CRUD operations

---
+++ CRUD/index.js
@@ -0,0 +1,11 @@
+// const fs = require('fs')
+// fs.writeFileSync("robo.txt","my name is tayyab")
+// fs.appendFileSync("robo.txt","I am in class 14")
+// fs.mkdirSync("tayyab")
+// fs.writeFileSync('tayyab/robo.txt','my name is tayyab hussain')
+// fs.appendFileSync('tayyab/robo.txt','. I am 19 years old')
+// fs.renameSync('tayyab/robo.txt',"tayyab/myFile.txt")
+// console.log(fs.readFileSync('tayyab/myFile.txt')) ***Return Buffer Data***
+// console.log(fs.readFileSync('tayyab/myFile.txt',"utf8"))
+// fs.unlinkSync('tayyab/myFile.txt')
+// fs.rmdirSync('tayyab')
