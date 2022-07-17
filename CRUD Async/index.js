9b87a2db7568f4e8a38adcbbc53bda730494e84f
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2022-07-17 14:38:11 +0500

    CRUD async operations

---
+++ CRUD Async/index.js
@@ -0,0 +1,32 @@
+// const fs = require('fs')
+// fs.writeFile('read.tx', 'hello world', (error) => {
+//     console.log(error)
+// })
+
+// fs.mkdir('myDir',(error)=>{
+//     console.log(error)
+// })
+
+// fs.writeFile('myDir/read.txt',"hello Wolrd",(error)=>{
+//     console.log(error)
+// })
+
+// fs.appendFile('myDir/read.txt',". Hello Program",(error)=>{
+//     console.log(error)
+// })
+
+// fs.readFile('myDir/read.txt',"utf-8",(error,data)=>{
+// console.log(data)
+// })
+
+// fs.rename('myDir/read.txt','myDir/myFile.text',(error)=>{
+//     console.log(error)
+// })
+
+// fs.unlink('myDir/myFile.text',(error)=>{
+//     console.log(error)
+// })
+
+// fs.rmdir("myDir",(error)=>{
+//     console.log(error)
+// })
